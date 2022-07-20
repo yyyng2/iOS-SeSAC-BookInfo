33671bce7ea1213d8966714b634b18b90685ce16
yyyng2 <yyyng2@gmail.com>
2022-07-20 16:56:35 +0900

    added

---
+++ BookInfo/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  BookInfo
+//
+//  Created by Y on 2022/07/20.
+//
+
+import Foundation
+import UIKit
+
+struct BookList{
+    var bookTitle: String
+    var bookRelease: String
+    var bookPages: Int
+    var bookOverview: String
+    var bookRate: Double
+    var bookAuthor: String
+    var bookCover: UIImage?
+}
